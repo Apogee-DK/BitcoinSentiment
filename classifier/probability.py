72b51b88b74bf15956b7f3fbe2c00a23e75ff408
Dexter K <dkwok.90@gmail.com>
2016-09-17 03:00:32 -0400

    Probability function
    Finalized the computation of the occurences of words in a tweet by
    checking the dataset

--- classifier/probability.py
+++ classifier/probability.py
@@ -1,30 +1,60 @@
 import parseTweet
 
 # Retrieves data from database
-import dbConvertToPython
+from dbStatistics import *
 
+def getWordOccurence():
+    # dbStatistics function that returns a list of 'key':[pos, neu, neg]
+    return getKeyAndValue()
 
-def getData():
-    wordOccurenceValues = []
-    # dbConvertToPython goes here
+def calculateSentiment(_words, _wordOccurenceValues):
 
-    return array;
+    tweetWordProbability = []
 
+    probabilityOfFeatures = 1
+    probOfFeatureWithKnownValue = 1
+    
+    # dbStatistics will handle the following functions
+    # P ( Value )
+    totalNumberOfPositiveTweets = getNumberOfPositiveTweets()
+    totalNumberOfTweets = getNumberOfTweets()
 
-def getWordProbability():
-
-
-
-
-
-
-def getTweetSentiment(tweetObject):
-    # Get necessary values from TweetObject
-    words = tweetObject.getWords()
-
-    for word in words:
-        getWordProbability(word)
+    probabilityOfValue = totalNumberOfPositiveTweets/totalNumberOfTweets
+    
+    # P ( Feature )
+    for word in _words:
+        listOfValues = _wordOccurenceValues[word]
+        sum = 0
         
+        for value in listOfValues:
+            sum += value
 
+        tweetWordProbability.append(sum/totalNumberOfTweets)
 
+    
+    for probability in tweetWordProbability:
+        probabilityOfFeatures *= probability
 
+    # P ( Feature | Value )
+    for word in _words:
+        listOfValues = _wordOccurenceValues[word]
+        
+        # Maximum value of the list
+        if(max(listOfValues) == listOfValues[neu])
+            continue
+        
+        # Getting only the positive value
+        value = listOfValues[pos] 
+        probabilityOfFeatureWithKnownValue *= value/totalNumberOfPositiveTweets
+
+    return (probabilityOfValue * probabilityOfFeatureWithKnownValue)/probilityOfFeatures
+
+def getTweetSentiment(_tweetObject):
+    # Get dictionary of <word, list of values (pos, neu, neg)>
+    wordOccurenceValues = getWordOccurence()
+    
+    # Get necessary values from TweetObject
+    words = _tweetObject.getWords()
+
+    # Determine the total value of the tweet
+    return calculateSentiment(words, wordOccurenceValues)
