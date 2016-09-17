46705a77fba3f909b63828be18aec9f6bfe14df8
Dexter K <dkwok.90@gmail.com>
2016-09-17 01:45:28 -0400

    Classifier object
    Created a python script that will handle the association of numerical
    value to each word and a sentimental value to the tweet

---
+++ classifier/probability.py
@@ -0,0 +1,30 @@
+import parseTweet
+
+# Retrieves data from database
+import dbConvertToPython
+
+
+def getData():
+    array = []
+    # dbConvertToPython goes here
+
+    return array;
+
+
+def getWordProbability():
+
+
+
+
+
+
+def getTweetSentiment(tweetObject):
+    # Get necessary values from TweetObject
+    words = tweetObject.getWords()
+
+    for word in words:
+        getWordProbability(word)
+        
+
+
+
